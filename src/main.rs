f4f3006d51243c5b7048464a46e189197dcfed1a
Jake Sandler <jsandler18@gmail.com>
2017-07-06 18:09:33 -0400

    Added command line argument parser

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate rand;
 extern crate sha1;
 #[macro_use]
 extern crate bitflags;
+extern crate clap;
 mod bencode;
 mod meta;
 mod tracker;
@@ -15,11 +16,25 @@ mod error;
 
 use meta::MetaInfo;
 use meta::FileInfo::Single;
+use clap::{App,Arg};
 
 fn main() {
-    let file1 = "/home/jake/boostrs/ubuntu-17.04-desktop-amd64.iso.torrent";
-    let file2 = "/home/jake/boostrs/research_paper_UDP.torrent";
-    match  meta::MetaInfo::parse_meta(file1) {
+    //get command line arguments
+    let args = App::new(env!("CARGO_PKG_NAME"))
+                        .version(env!("CARGO_PKG_VERSION"))
+                        .author(env!("CARGO_PKG_AUTHORS"))
+                        .about("A torrent client written in rust")
+                        .arg(
+                            Arg::with_name("meta")
+                                .required(true)
+                                .short("m")
+                                .long("meta")
+                                .takes_value(true)
+                                .help("The torrent's metafile")
+                        ).get_matches();
+
+    let file = args.value_of("meta").unwrap();
+    match  meta::MetaInfo::parse_meta(file) {
         Ok(MetaInfo { announce_url, piece_len:_, info_hash, piece_hashes: _, file_info: Single {filename:_, filelength}} ) => {
             let peerid = "-BO1000-001234567890".as_bytes(); 
             match tracker::TrackerInfo::tracker_request(announce_url.as_str(), &info_hash, peerid, 12345, 0, 0, filelength, tracker::TrackerEvent::Started, None) {
